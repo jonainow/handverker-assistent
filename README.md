fd6745325aaabe595ee0febc698cf1101db739c8
jonainow <jon.ainow@gmail.com>
2025-06-17 20:23:31 +0200

    Utvidet materialvalg og lagt til bjelkeberegning

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # handverker-assistent
-Et beregningsverktøy for håndverkere med enkeltmansforetak
+Et beregningsverktøy for håndverkere med enkeltmannsforetak.
+
+## Funksjoner
+
+- Beregning av veggareal
+- Beregning av antall gipsplater
+- Materialanbefaling basert på konstruksjon og miljø
+- Enkel dimensjonering av bjelker i tre, limtre og stål
--- app/beregning/materialvalg.py
+++ app/beregning/materialvalg.py
@@ -6,18 +6,24 @@ def anbefal_materiale(konstruksjonstype, miljø="tørt"):
 
     if konstruksjonstype == "innervegg":
         if miljø == "våtrom":
-            return "Anbefalt: Våtromsplater (f.eks. Litex eller ProPlate)"
+            return "Anbefalt: Våtromsplater eller impregnert treverk"
         else:
-            return "Anbefalt: Gipsplater eller trepanel"
+            return "Anbefalt: Gipsplater, trepanel eller stålstendere"
 
     elif konstruksjonstype == "yttervegg":
-        return "Anbefalt: Vindtettplate + isolasjon + kledning"
+        return "Anbefalt: Vindtettplate, isolasjon og kledning av tre eller stål"
 
     elif konstruksjonstype == "bærende vegg":
-        return "Anbefalt: Konstruksjonsvirke (f.eks. 48x148 / 48x198)"
+        return "Anbefalt: Konstruksjonsvirke eller limtre"
 
     elif konstruksjonstype == "tak":
-        return "Anbefalt: Takstoler og undertak (spon eller OSB), evt. vindsperre"
+        return "Anbefalt: Takstoler av tre eller stål med undertak"
+
+    elif konstruksjonstype == "gulv":
+        if miljø == "våtrom":
+            return "Anbefalt: Impregnerte bjelker med gulvsponplater"
+        else:
+            return "Anbefalt: Bjelkelag av tre og gulvspon eller parkett"
 
     else:
         return "Ingen anbefaling tilgjengelig for denne typen."
